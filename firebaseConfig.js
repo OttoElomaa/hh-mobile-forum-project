92a155b087133f91a9edb7447b712c539e04511d
Otto Elomaa <otto.elomaa99@gmail.com>
2025-04-10 11:30:11 +0300

    Google Firebase setup done
    Created a Firebase project with Real-time database and Web app in it. Added firebase config file based on the project. Added metro config

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYhw0EXtPDMjkCMU5Te9LcZjv4v3dq_hg",
+  authDomain: "mobile-forum-project.firebaseapp.com",
+  databaseURL: "https://mobile-forum-project-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "mobile-forum-project",
+  storageBucket: "mobile-forum-project.firebasestorage.app",
+  messagingSenderId: "227130559860",
+  appId: "1:227130559860:web:c7a9ce8903ca7ce0d53021"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
